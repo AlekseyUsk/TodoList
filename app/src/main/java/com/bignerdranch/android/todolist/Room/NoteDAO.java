0bcd0d88fd00e12bb9521bc724b301fb3e28ffb0
Aleksey <meysonsib200189@mail.ru>
2024-03-02 13:57:13 +0700

    добавил Обязательную обработку ошибок в Consumer

--- app/src/main/java/com/bignerdranch/android/todolist/Room/NoteDAO.java
+++ app/src/main/java/com/bignerdranch/android/todolist/Room/NoteDAO.java
@@ -9,13 +9,12 @@ import androidx.room.Query;
 import java.util.List;
 
 import io.reactivex.rxjava3.core.Completable;
-import io.reactivex.rxjava3.core.Single;
 
 @Dao
 public interface NoteDAO {
 
     @Query("SELECT * FROM notes")
-    Single<List<Note>> getNote();
+    LiveData<List<Note>> getNote();
 
     @Insert
     Completable addNote(Note note);
--- app/src/main/java/com/bignerdranch/android/todolist/presentation/viewmodel/ViewModel.java
+++ app/src/main/java/com/bignerdranch/android/todolist/presentation/viewmodel/ViewModel.java
@@ -1,20 +1,20 @@
 package com.bignerdranch.android.todolist.presentation.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 
 import com.bignerdranch.android.todolist.Room.Note;
 import com.bignerdranch.android.todolist.Room.NoteDataBase;
 
 import java.util.List;
 
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
@@ -22,8 +22,6 @@ public class ViewModel extends AndroidViewModel {
 
     private NoteDataBase noteDataBase;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
-    private MutableLiveData<List<Note>> notes = new MutableLiveData<>();
-
 
     public ViewModel(@NonNull Application application) {
         super(application);
@@ -31,27 +29,23 @@ public class ViewModel extends AndroidViewModel {
     }
 
     public LiveData<List<Note>> getNotes() {
-        return notes;
-    }
-
-    public void updatingTheData() {
-        Disposable disposable = noteDataBase.noteDAO().getNote()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<List<Note>>() {
-                    @Override
-                    public void accept(List<Note> notesFromDataBase) throws Throwable {
-                        notes.setValue(notesFromDataBase);
-                    }
-                });
-        compositeDisposable.add(disposable);
+        return noteDataBase.noteDAO().getNote();
     }
 
     public void remove(Note note) {
         Disposable disposable = noteDataBase.noteDAO().removeNote(note.getId())
                 .subscribeOn(Schedulers.io())
-                .subscribe();
-        updatingTheData();
+                .subscribe(new Action() {
+                    @Override
+                    public void run() throws Throwable {
+                        Log.d("ViewModel", "removed :" + note.getId());
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Throwable {
+                        Log.d("ViewModel", "error remove");
+                    }
+                });
         compositeDisposable.add(disposable);
     }
 
